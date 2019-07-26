e2b976e3f742700173264b50b937e00770d25af3
Tony <Zylphrex@users.noreply.github.com>
2019-07-26 10:50:33 -0700

    feat: Added get function for convenience (#7)

--- gkms/__init__.py
+++ gkms/__init__.py
@@ -1,2 +1,10 @@
 from gkms.cmd.decrypt import decrypt
 from gkms.cmd.encrypt import encrypt
+from gkms.cmd.reencrypt import reencrypt
+
+
+def get(project, bucket, target, default=None):
+    try:
+        return decrypt(project, bucket, target)
+    except ValueError:
+        return default
